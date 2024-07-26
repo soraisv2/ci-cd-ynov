92fa7e4f3f01d54f63b50436c093563f3497bb47
Loïse Fenoll <admin@fingertips.fr>
2024-07-26 10:19:00 +0200

    skip cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io')
   })
 
-  it('deployed react app to localhost', () => {
+  it.skip('deployed react app to localhost', () => {
     cy.visit('http://localhost:3000')
     cy.contains('1 user(s) already registered')
   })
