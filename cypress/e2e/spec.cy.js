d1d30fae8a3644fb1cff3f666f22ca3a0d21a4ce
Loïse Fenoll <admin@fingertips.fr>
2024-10-11 13:10:43 +0200

    Update dependencies and adding target to publish to npm

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,27 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('deployed react app to localhost', () => {
+    cy.intercept('GET', 'https://ci-cd-back-ynov.vercel.app/users', {
+      statusCode: 200,
+      body: {
+        utilisateurs: [
+          {
+            id: '1',
+            nom: 'a',
+            prenom: 'b',
+            email: 'c@c.fr'
+          },
+          {
+            id: '2',
+            nom: 'a',
+            prenom: 'b',
+            email: 'c@c.fr'
+          }
+        ],
+      },
+    });
+
+    cy.visit('http://localhost:3000')
+    cy.contains('2 user(s) already registered')
   })
 
-  it.skip('deployed react app to localhost', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('1 user(s) already registered')
-  })
-  
 })
